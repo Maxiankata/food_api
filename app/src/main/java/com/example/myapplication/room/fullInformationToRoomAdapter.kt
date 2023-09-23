a4ddb11f7799a9d4730234694464ab0e17d80c69
Max <maxnenchev@gmail.com>
2023-09-23 19:29:08 +0300

    IM DATABASING THIS NIGGA

---
+++ app/src/main/java/com/example/myapplication/room/fullInformationToRoomAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.room
+
+import com.example.myapplication.adapters.Adapter
+import com.example.myapplication.data.FrontFood
+import com.example.myapplication.data.Response
+
+
+class FullInformationToRoomAdapter: Adapter<Response.FullInformationRecipe, FoodRoomInfo> {
+    override fun adapt(t: Response.FullInformationRecipe): FoodRoomInfo? {
+        return FoodRoomInfo(t.id, t.title!!, t.image!!)
+    }
+}
+class RoomToFullInformationAdapter:Adapter<FoodRoomInfo, Response.FullInformationRecipe>{
+    override fun adapt(t: FoodRoomInfo): Response.FullInformationRecipe? {
+        return Response.FullInformationRecipe(t.id, t.name!!, t.image!!)
+    }
+
+}
