0f48b7454d8b54d87caf4186e7a9571836a29f08
Max <maxnenchev@gmail.com>
2023-09-23 20:36:57 +0300

    random works, starting fix on viewpagers

--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.home
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -13,17 +14,20 @@ import kotlinx.coroutines.launch
 class HomeViewModel(application: Application): AndroidViewModel(application) {
 
 
-    private var api: RetrofitFoodApiService = application.getApiService()
+    private var api = application.getApiService()
     private val _recipe = MutableLiveData<FrontFood>()
+    private val _trivia = MutableLiveData<String>()
+    val trivia: LiveData<String> get() = _trivia
     val recipe: LiveData<FrontFood> get() = _recipe
-//    init {
-//        fetchRecipe()
-//    }
-//
-//    private fun fetchRecipe() {
-//        viewModelScope.launch {
-//            val result =
-//                api.getRandomRecipe()
-//        }
-//    }
+    init {
+        fetchRecipe()
+    }
+
+    private fun fetchRecipe() {
+        viewModelScope.launch {
+            val result = api.getRandomTrivia()
+            _trivia.postValue(result)
+            Log.d("TRIVIA SENT", result)
+        }
+    }
 }
