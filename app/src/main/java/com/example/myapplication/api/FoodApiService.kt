c2ec7af5f44ad3c1f8a16b1cba27ec281b6fcfc5
Max <maxnenchev@gmail.com>
2023-09-23 20:36:57 +0300

    random works, starting fix on viewpagers

--- app/src/main/java/com/example/myapplication/api/FoodApiService.kt
+++ app/src/main/java/com/example/myapplication/api/FoodApiService.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.api
 
 import com.example.myapplication.data.FrontFood
+import com.example.myapplication.data.ApiRandomTrivia
 import com.example.myapplication.data.Response.FullInformationRecipe
 import com.example.myapplication.data.Steps
 import com.example.myapplication.data.TextPredictor
@@ -8,11 +9,12 @@ import kotlinx.coroutines.flow.Flow
 
 interface FoodApiService {
     suspend fun getFoodByComplexSearch(query: String): List<FrontFood>
-    suspend fun getRandomRecipe(): FullInformationRecipe?
+    suspend fun getRandomRecipe(): List<FrontFood>
     suspend fun getPrediction(query: String):List<TextPredictor>
     suspend fun getRecipeById(id:Int): FullInformationRecipe
     suspend fun findByNutrients(minCarbs :Int, maxCarbs:Int ,minFat:Int , maxFat:Int , minCalories:Int , maxCalories:Int ,minProtein :Int, maxProtein:Int): List<FrontFood>
     suspend fun findByIngredients(query: String): List<FrontFood>
-    suspend fun getRecipeBulk(): Flow<List<FullInformationRecipe>>
     suspend fun getRecipeInstructionsById(id:Int):List<Steps>
+    suspend fun getRandomTrivia(): String
+
 }
