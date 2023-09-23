3cead43b81540f6a1d9ca4f19f6822bffe1f596d
Max <maxnenchev@gmail.com>
2023-09-23 10:25:23 +0300

    fkc

---
+++ app/src/main/java/com/example/myapplication/adapters/FoodToFrontFoodAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.adapters
+
+class FoodToFrontFoodAdapter {
+}
--- app/src/main/java/com/example/myapplication/api/FoodApiService.kt
+++ app/src/main/java/com/example/myapplication/api/FoodApiService.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication.api
 
-import com.example.myapplication.data.FrontFood
-import com.example.myapplication.data.Response.FullInformationRecipe
-import com.example.myapplication.data.TextPredictor
+import FrontFood
+import TextPredictor
+
+import kotlinx.coroutines.flow.Flow
 
 interface FoodApiService {
     suspend fun getFoodByComplexSearch(query: String): List<FrontFood>
-    suspend fun getRandomRecipe(): FullInformationRecipe
+    suspend fun getRandomRecipe(): Response.FullInformationRecipe
     suspend fun getPrediction(query: String):List<TextPredictor>
-    suspend fun getRecipeById(id:Int): FullInformationRecipe
+    suspend fun getRecipeById(id:Int): Response.FullInformationRecipe
+    suspend fun findByNutrients(minCarbs :Int, maxCarbs:Int ,minFat:Int , maxFat:Int , minCalories:Int , maxCalories:Int ,minProtein :Int, maxProtein:Int): List<FrontFood>
+    suspend fun findByIngredients(query: String): List<FrontFood>
+    suspend fun getRecipeBulk(): Flow<List<Response.FullInformationRecipe>>
+    suspend fun getRecipeInstructionsById(id:Int):List<Response.FullInformationRecipe.Steps>
 }
